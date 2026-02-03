28f243b895c6e8c7b372c10399527df38c8c9abd
mpk <mpk@process-factory.dk>
2026-02-03 09:19:14 +0100

    feat: added method for generating a random password

--- java-utils/src/main/java/dk/pekilidi/jasypt/Secrets.java
+++ java-utils/src/main/java/dk/pekilidi/jasypt/Secrets.java
@@ -1,6 +1,14 @@
 package dk.pekilidi.jasypt;
 
+import static org.passay.DigestDictionaryRule.ERROR_CODE;
+
+import java.util.List;
+
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.passay.CharacterData;
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.PasswordGenerator;
 
 public class Secrets {
     static StandardPBEStringEncryptor enc = new StandardPBEStringEncryptor();
@@ -14,6 +22,9 @@ public class Secrets {
 
     public static void init(){
         String masterPassword = System.getenv("JASYPT_ENCRYPTOR_PASSWORD");
+		if(masterPassword == null || masterPassword.isBlank()) {
+			masterPassword = System.getProperty("JASYPT_ENCRYPTOR_PASSWORD");
+		}
         if(masterPassword == null) {
             throw new IllegalStateException("JASYPT_ENCRYPTOR_PASSWORD environment variable is not set.");
         }
@@ -34,4 +45,30 @@ public class Secrets {
         }
         return enc.decrypt(encryptedValue);
     }
+
+	public static String generateRandomPassword(int length) {
+		PasswordGenerator gen = new PasswordGenerator();
+		CharacterRule lowerCaseRule = new CharacterRule(EnglishCharacterData.LowerCase);
+		lowerCaseRule.setNumberOfCharacters(2);
+		CharacterRule upperCaseRule = new CharacterRule(EnglishCharacterData.UpperCase);
+		upperCaseRule.setNumberOfCharacters(2);
+
+		CharacterRule digitRule = new CharacterRule(EnglishCharacterData.Digit);
+		digitRule.setNumberOfCharacters(2);
+
+		CharacterData specialChars = new CharacterData() {
+			public String getErrorCode() {
+				return ERROR_CODE;
+			}
+
+			public String getCharacters() {
+				return "!@#$%^&*()_+";
+			}
+		};
+
+		CharacterRule splCharRule = new CharacterRule(specialChars);
+		splCharRule.setNumberOfCharacters(2);
+
+		return gen.generatePassword(length, List.of( lowerCaseRule, upperCaseRule, digitRule, splCharRule));
+	}
 }
